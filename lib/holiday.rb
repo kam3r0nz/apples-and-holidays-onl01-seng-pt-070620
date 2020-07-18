4b23b732e3a5c25ddfb1e887853db834212907c5
Kameron Hungerford <kam.hungerford@gmail.com>
2020-07-18 01:25:21 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -58,7 +58,7 @@ def all_winter_holiday_supplies(holiday_hash)
   holiday_hash[:winter].map do |holiday, supply|
     supply
   end
-  array.flatten
+  holiday_hash.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
