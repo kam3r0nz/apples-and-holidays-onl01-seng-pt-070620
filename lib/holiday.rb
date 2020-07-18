68deab4ce606810633d5e90ca00f3ee77e8b0c62
Kameron Hungerford <kam.hungerford@gmail.com>
2020-07-18 01:41:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -82,7 +82,8 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday|
     holiday.each do |holiday, supply|
       holiday if supply.include?("BBQ")
-
+    end
+  end
 end
 
 
