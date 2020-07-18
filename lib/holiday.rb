bccc78623476990b60759fd6550f18b9297d2fcf
Kameron Hungerford <kam.hungerford@gmail.com>
2020-07-18 01:25:07 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -55,7 +55,7 @@ end
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
 
-  holiday_hash[:winter].map.flatten do |holiday, supply|
+  holiday_hash[:winter].map do |holiday, supply|
     supply
   end
   array.flatten
