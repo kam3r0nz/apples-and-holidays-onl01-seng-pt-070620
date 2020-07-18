091a0195560c0726edf3c02726e4c17d171c8ca2
Kameron Hungerford <kam.hungerford@gmail.com>
2020-07-18 01:31:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 
-  holiday_hash.each do |season, supply|
+  holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
     holiday.each do |holiday, item|
       puts " #{holiday.to_s.split("").map {|i| i.capitalize}.join("")}: #{item.join(",")}"
