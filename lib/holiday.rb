bf7d128c17d3847454906045ef1362a77370c49c
Kameron Hungerford <kam.hungerford@gmail.com>
2020-07-18 01:27:32 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,8 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 
-  holiday_hash
+  holiday_hash.each do |holiday, supply|
+    
 end
 
 def all_holidays_with_bbq(holiday_hash)
