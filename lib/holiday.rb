d795c9210505db4dd7972bc00d7a9e32c1902908
Kameron Hungerford <kam.hungerford@gmail.com>
2020-07-18 01:12:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -28,6 +28,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   
   holiday_hash[:winter][:christmas][:supplies] = "Balloons"
+  holiday_hash[:winter][:new_years][:supplies] = "Balloons"
   
 
 end
