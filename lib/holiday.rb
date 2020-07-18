132c3142d7ba04a9a348f519b18dc8b591ae3505
Kameron Hungerford <kam.hungerford@gmail.com>
2020-07-18 01:28:56 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -70,7 +70,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |holiday, supply|
     puts "#{season.capitalize}:"
     holiday.each do |holiday, item|
-      puts " #{holiday.to_s}"
+      puts " #{holiday.to_s.split}"
 end
 
 def all_holidays_with_bbq(holiday_hash)
